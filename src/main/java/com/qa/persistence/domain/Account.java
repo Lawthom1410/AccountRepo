6c4a234543ab6a30426d0741e3debaa92e0f4951
Lawthom1410 <jack.lawthom@academytrainee.com>
2019-07-16 12:36:46 +0100

    second commit

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -2,6 +2,11 @@ package com.qa.persistence.domain;
 
 public class Account {
 	
+	private int accountid;
+	private int accountNum;
+	private String firstName;
+	private String lastName;
+	
 	//This class needs to have:
 	//An id
 	//An Account Number
